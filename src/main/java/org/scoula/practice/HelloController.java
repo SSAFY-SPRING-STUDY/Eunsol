ca58992811139625708e631c3a75ea7b2bbed9e3
yel0732 <yerilee1210@gmail.com>
2026-04-01 11:20:10 +0900

    feat: Implement HelloController and HelloService

---
+++ src/main/java/org/scoula/practice/HelloController.java
@@ -0,0 +1,19 @@
+package org.scoula.practice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private final HelloService helloService;
+
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String hi(){
+        return helloService.hi();
+    }
+}
