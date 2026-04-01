663844a0a35940b2ee97932666f8d25cd7279004
yel0732 <yerilee1210@gmail.com>
2026-04-01 15:57:56 +0900

    feat: Implement Post CRUD API with memory-based storage

---
+++ src/main/java/org/scoula/practice/repository/PostRepository.java
@@ -0,0 +1,30 @@
+package org.scoula.practice.repository;
+
+import org.scoula.practice.entity.PostEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class PostRepository {
+    List<PostEntity> postList = new ArrayList<>();
+
+    public PostEntity save(PostEntity postEntity) {
+        postList.add(postEntity);
+        return postEntity;
+    }
+
+    public List<PostEntity> findAll() {
+        return postList;
+    }
+
+    public Optional<PostEntity> findById(Long id) {
+        return postList.stream().filter(post -> post.getId().equals(id)).findAny();
+    }
+
+    public void deleteById(Long id) {
+        postList.removeIf(post -> post.getId().equals(id));
+    }
+}
